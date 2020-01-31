fd32bc4e0cd8957ea7d41287aa8d6c8bde5eedeb
Vincent <enyinwa.vincent@gmail.com>
2020-01-31 18:06:53 +0100

    new branch (middlewaresuppoer): working on middleware support

--- app/server/server.go
+++ app/server/server.go
@@ -5,11 +5,15 @@ import (
 	"net/http"
 
 	"github.com/MrVxCo/go-http-server/app/route"
+	"github.com/urfave/negroni"
 )
 
 //StartServer starts the go server
 func StartServer() {
 	r := route.LoadRoutes()
+	middlewares := negroni.New()
+	// middlewares.Use()
+	middlewares.UseHandler(r)
 
 	PORT := "2200"
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/MrVxCo/go-http-server
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/urfave/negroni v1.0.0
+)
